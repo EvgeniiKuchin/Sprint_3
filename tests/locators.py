66b8dd71c82fd4cd251c7aec9e22cddb6a3b1d67
Evgenii Kuchin <neon-power2@yandex.ru>
2023-04-27 12:14:27 +0300

    add_conftest and new_locators

--- tests/locators.py
+++ tests/locators.py
@@ -1,20 +1,20 @@
 
 
 class Locators:
-    BUTTON_IN_ACCOUNT = "/html[1]/body[1]/div[1]/div[1]/main[1]/section[2]/div[1]/button[1]" # Большая кнопка войти в аккаунт на главной транице
-    ELEMENT_IN_MAIL = "//body/div[@id='root']/div[1]/main[1]/div[1]/form[1]/fieldset[1]/div[1]/div[1]/input[1]" # Строка ввода емэил
-    ELELENT_IN_PASS = "/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/form[1]/fieldset[2]/div[1]/div[1]/input[1]"# Строка ввода пароля
-    BUTTON_SING_IN = "/html[1]/body[1]/div[1]/div[1]/main[1]/div[1]/form[1]/button[1]" # Кнопка войти
-    BUTTON_PERSONAL_AREA = "//header/nav[1]/a[1]/*[1]" # Кнопка личный кабинет
+    BUTTON_IN_ACCOUNT = "//button[contains(text(), 'Войти в аккаунт') and contains(@class, 'button_button__33qZ0') and contains(@class, 'button_button_type_primary__1O7Bx') and contains(@class, 'button_button_size_large__G21Vg')]"# Большая кнопка войти в аккаунт на главной транице
+    ELEMENT_IN_MAIL = "//input[@class='text input__textfield text_type_main-default' and @name='name']" # Строка ввода емэил
+    ELELENT_IN_PASS = "//input[@class='text input__textfield text_type_main-default' and @type='password']"# Строка ввода пароля
+    BUTTON_SING_IN = "//button[@class='button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa' and text()='Войти']" # Кнопка войти
+    BUTTON_PERSONAL_AREA = "//a[@class='AppHeader_header__link__3D_hX' and @href='/account']" # Кнопка личный кабинет
     TEXT_PROFILE = "//a[contains(text(),'Профиль')]" # Текст на странице "Профиль"
     BUTTON_IN_ACCOUNT_FORM = "//a[contains(text(),'Войти')]" # Маленькая кнопка Войти на форме регистрации
     BUTTON_CONSTRUCTOR = "//p[contains(text(),'Конструктор')]" #
     BUTTON_MAIN_LOGO = "//header/nav[1]/div[1]/a[1]/*[1]" #
     BUTTON_SOUS = "//span[contains(text(),'Соусы')]" # Кнопка соусы
     TEXT_SOUS = "//h2[contains(text(),'Соусы')]" # Текст соусы
-    FILLINGS = "//span[contains(text(),'Начинки')]" # Кнопка начинки
+    FILLINGS = "/html[1]/body[1]/div[1]/div[1]/main[1]/section[1]/div[1]/div[3]/span[1]" # Кнопка начинки
     TEXT_FILLINGS = "//h2[contains(text(),'Начинки')]" # Текст начинки
-    BAGEL = "//span[contains(text(),'Булки')]" # Кнопка булки
+    BAGEL = "/html[1]/body[1]/div[1]/div[1]/main[1]/section[1]/div[1]/div[1]/span[1]" # Кнопка булки
     TEXT_BAGEL = "//h2[contains(text(),'Булки')]" # Текст булки
     ELELENT_NAME_REGISTRATION = "//body/div[@id='root']/div[1]/main[1]/div[1]/form[1]/fieldset[1]/div[1]/div[1]/input[1]"
     ELELENT_EMAIL_REGISTRATION = "//body/div[@id='root']/div[1]/main[1]/div[1]/form[1]/fieldset[2]/div[1]/div[1]/input[1]"
--- tests/tests_account_registration.py
+++ tests/tests_account_registration.py
@@ -4,81 +4,83 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from locators import Locators
 import random
+
+
 class Test_account_rg:
-    def test_registration_new_user_data_all_correct(self):
-        driver = webdriver.Chrome()
-        driver.get("https://stellarburgers.nomoreparties.site/register")
-        name_field = driver.find_element(By.XPATH, Locators.ELELENT_NAME_REGISTRATION)
+    def test_registration_new_user_data_all_correct(self, browser):
+
+        browser.get("https://stellarburgers.nomoreparties.site/register")
+        name_field = browser.find_element(By.XPATH, Locators.ELELENT_NAME_REGISTRATION)
         rando_name = "Test_Use" + str(random.randint(1, 99999))
         rando_pass = random.randint(100000, 999999)
         rando_email = "evgnii" + str(random.randint(1, 9999)) + "@ya.ru"
         name_field.send_keys(rando_name)
-        email_field = driver.find_element(By.XPATH, Locators.ELELENT_EMAIL_REGISTRATION)
+        email_field = browser.find_element(By.XPATH, Locators.ELELENT_EMAIL_REGISTRATION)
         email_field.send_keys(rando_email)
-        password_field = driver.find_element(By.XPATH, Locators.ELELENT_PASSWORD_REGISTTATION)
+        password_field = browser.find_element(By.XPATH, Locators.ELELENT_PASSWORD_REGISTTATION)
         password_field.send_keys(rando_pass)
-        driver.find_element(By.XPATH, Locators.BUTTON_REGISTER).click()
+        browser.find_element(By.XPATH, Locators.BUTTON_REGISTER).click()
 
-        driver.get("https://stellarburgers.nomoreparties.site/login")
-        email_field = driver.find_element(By.XPATH, Locators.ELEMENT_IN_MAIL)
+        browser.get("https://stellarburgers.nomoreparties.site/login")
+        email_field = browser.find_element(By.XPATH, Locators.ELEMENT_IN_MAIL)
         email_field.send_keys(rando_email)
-        password_field = driver.find_element(By.XPATH, Locators.ELELENT_IN_PASS)
+        password_field = browser.find_element(By.XPATH, Locators.ELELENT_IN_PASS)
         password_field.send_keys(rando_pass)
-        driver.find_element(By.XPATH, Locators.BUTTON_SING_IN).click()
+        browser.find_element(By.XPATH, Locators.BUTTON_SING_IN).click()
 
-        driver.find_element(By.XPATH, Locators.BUTTON_PERSONAL_AREA).click()
-        WebDriverWait(driver, 3).until(
+        browser.find_element(By.XPATH, Locators.BUTTON_PERSONAL_AREA).click()
+        WebDriverWait(browser, 3).until(
             expected_conditions.presence_of_element_located((By.XPATH, Locators.TEXT_PROFILE)))
-        assert driver.find_element(By.XPATH, Locators.TEXT_PROFILE).text == 'Профиль'
+        assert browser.find_element(By.XPATH, Locators.TEXT_PROFILE).text == 'Профиль'
 
-    def test_login_verification_successfully(self):
-        driver = webdriver.Chrome()
-        driver.get("https://stellarburgers.nomoreparties.site/login")
-        email_field = driver.find_element(By.XPATH, Locators.ELEMENT_IN_MAIL)
+    def test_login_verification_successfully(self, browser):
+
+        browser.get("https://stellarburgers.nomoreparties.site/login")
+        email_field = browser.find_element(By.XPATH, Locators.ELEMENT_IN_MAIL)
         email_field.send_keys("12345@ya.ru")
-        password_field = driver.find_element(By.XPATH, Locators.ELELENT_IN_PASS)
+        password_field = browser.find_element(By.XPATH, Locators.ELELENT_IN_PASS)
         password_field.send_keys("123456")
-        driver.find_element(By.XPATH, Locators.BUTTON_SING_IN).click()
-        driver.find_element(By.XPATH, Locators.BUTTON_PERSONAL_AREA).click()
-        WebDriverWait(driver, 3).until(
+        browser.find_element(By.XPATH, Locators.BUTTON_SING_IN).click()
+        browser.find_element(By.XPATH, Locators.BUTTON_PERSONAL_AREA).click()
+        WebDriverWait(browser, 3).until(
             expected_conditions.presence_of_element_located((By.XPATH, Locators.TEXT_PROFILE)))
-        assert driver.find_element(By.XPATH, Locators.TEXT_PROFILE).text == 'Профиль'
-        driver.quit()
+        assert browser.find_element(By.XPATH, Locators.TEXT_PROFILE).text == 'Профиль'
 
-    def test_registration_new_user_incorrect_password(self):
-        driver = webdriver.Chrome()
-        driver.get("https://stellarburgers.nomoreparties.site/register")
+
+    def test_registration_new_user_incorrect_password(self, browser):
+
+        browser.get("https://stellarburgers.nomoreparties.site/register")
         # Заполнить поля для регистрации
-        name_field = driver.find_element(By.XPATH,Locators.ELELENT_NAME_REGISTRATION)
+        name_field = browser.find_element(By.XPATH,Locators.ELELENT_NAME_REGISTRATION)
         name_field.send_keys("Test_User")
-        email_field = driver.find_element(By.XPATH,Locators.ELELENT_EMAIL_REGISTRATION)
+        email_field = browser.find_element(By.XPATH,Locators.ELELENT_EMAIL_REGISTRATION)
         email_field.send_keys("123@ya.ru")
-        password_field = driver.find_element(By.XPATH,Locators.ELELENT_PASSWORD_REGISTTATION)
+        password_field = browser.find_element(By.XPATH,Locators.ELELENT_PASSWORD_REGISTTATION)
         password_field.send_keys("12345")
-        driver.find_element(By.XPATH,Locators.BUTTON_REGISTER).click()
-        WebDriverWait(driver, 3).until(expected_conditions.presence_of_element_located((By.XPATH, Locators.TEXT_INCORRECT_PASSWORD)))
+        browser.find_element(By.XPATH,Locators.BUTTON_REGISTER).click()
+        WebDriverWait(browser, 3).until(expected_conditions.presence_of_element_located((By.XPATH, Locators.TEXT_INCORRECT_PASSWORD)))
 
-        assert driver.find_element(By.XPATH, Locators.TEXT_INCORRECT_PASSWORD).text == 'Некорректный пароль'
+        assert browser.find_element(By.XPATH, Locators.TEXT_INCORRECT_PASSWORD).text == 'Некорректный пароль'
 
-        driver.quit()
 
-    def test_logout(self):
-        driver = webdriver.Chrome()
-        driver.get("https://stellarburgers.nomoreparties.site/login")
-        email_field = driver.find_element(By.XPATH, Locators.ELEMENT_IN_MAIL)
+
+    def test_logout(self, browser):
+
+        browser.get("https://stellarburgers.nomoreparties.site/login")
+        email_field = browser.find_element(By.XPATH, Locators.ELEMENT_IN_MAIL)
         email_field.send_keys("12345@ya.ru")
-        password_field = driver.find_element(By.XPATH, Locators.ELELENT_IN_PASS)
+        password_field = browser.find_element(By.XPATH, Locators.ELELENT_IN_PASS)
         password_field.send_keys("123456")
-        driver.find_element(By.XPATH, Locators.BUTTON_SING_IN).click()
+        browser.find_element(By.XPATH, Locators.BUTTON_SING_IN).click()
 
-        driver.find_element(By.XPATH, Locators.BUTTON_PERSONAL_AREA).click()
-        WebDriverWait(driver, 3).until(
+        browser.find_element(By.XPATH, Locators.BUTTON_PERSONAL_AREA).click()
+        WebDriverWait(browser, 3).until(
             expected_conditions.presence_of_element_located((By.XPATH, Locators.TEXT_PROFILE)))
-        WebDriverWait(driver, 3).until(expected_conditions.presence_of_element_located((By.XPATH, Locators.BUTTON_LOG_OUT)))
-        driver.find_element(By.XPATH, Locators.BUTTON_LOG_OUT).click()
-        WebDriverWait(driver, 3).until(expected_conditions.presence_of_element_located((By.XPATH, Locators.TEXT_ENTRANCE)))
+        WebDriverWait(browser, 3).until(expected_conditions.presence_of_element_located((By.XPATH, Locators.BUTTON_LOG_OUT)))
+        browser.find_element(By.XPATH, Locators.BUTTON_LOG_OUT).click()
+        WebDriverWait(browser, 3).until(expected_conditions.presence_of_element_located((By.XPATH, Locators.TEXT_ENTRANCE)))
 
-        assert driver.find_element(By.XPATH, Locators.TEXT_ENTRANCE).text == 'Вход'
-        driver.quit()
+        assert browser.find_element(By.XPATH, Locators.TEXT_ENTRANCE).text == 'Вход'
+
 
 
